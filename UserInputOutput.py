823404c5946c05cad56463b605482da2d6c6252b
tasuja <sujata.niroula750@gmail.com>
2017-07-21 16:09:29 +0545

    First comit

---
+++ UserInputOutput.py
@@ -0,0 +1,8 @@
+nm=(input('Enter your name: '))
+a=(input('Enter your age: '))
+g=(input('Enter your gender: '))
+ad=(input('Enter your address: '))
+print('Name =' +nm +'\n','Age = '+a +'\n','Gender = '+g +'\n','Address = '+ad)
+
+
+
--- multiplicationTable.py
+++ multiplicationTable.py
@@ -1,7 +1,7 @@
 a=int(input('Please enter a positive integer: '))
-print 'The multiplication table is:'
+print ('The multiplication table is:')
 for i in range (1,11):
    #print'{:2} *{:2} = {:2}'.format(a,i,a*i)
-    print a+'x'+i+'='+a*i
+    print (a+'x'+i+'='+a*i)
  
         
